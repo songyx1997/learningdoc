1a2d3f784286d9da27e8e027baa4c5c98f64c9ff
songyx1997 <songyunxiang1997@163.com>
2024-03-05 00:02:49 +0800

    宏任务与微任务的执行案例

---
+++ leetcode/宏任务与微任务/test2.js
@@ -0,0 +1,22 @@
+async function test1() {
+    console.log('1');
+    console.log(await test2());
+    console.log('2');
+}
+async function test2() {
+    console.log('3');
+    return '4';
+}
+test1();
+console.log('5');
+setTimeout(() => {
+    console.log('6');
+}, 0);
+new Promise((resolve) => {
+    console.log('7');
+    resolve();
+}).then(() => {
+    console.log('8');
+});
+console.log('9');
+// 1 3 5 7 9 4 2 8 6
