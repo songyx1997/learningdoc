2c7e37108ade91e7848d1c145cbc2c1b12bae222
songyx1997 <songyunxiang1997@163.com>
2024-03-09 13:10:02 +0800

    无重复的最长子串

--- leetcode/README.md
+++
@@ -1,7 +0,0 @@
-<table>
-    <tr>
-        <td>title：leetcode</td>
-        <td>author：songyx</td>
-        <td>date：2024/03/09</td>
-    </tr>
-</table>
---
+++ leetcode/string/1.lengthOfLongestSubstring.js
@@ -0,0 +1,25 @@
+/* 给定一个字符串s，请你找出其中不含有重复字符的最长子串的长度。
+核心思想：利用滑动窗口。 */
+const lengthOfLongestSubstring = function (str) {
+    let window = [];
+    let max = 0;
+    for (const s of str) {
+        let index = window.indexOf(s);
+        if (index == -1) {
+            window.push(s);
+        } else {
+            window = window.slice(index + 1);
+            window.push(s);
+        }
+        console.log(window);
+        if (max < window.length) {
+            max = window.length
+        }
+    }
+    return max;
+};
+
+/* [ 'a', 'd', 'v' ]
+当数组中添加新元素d时，出现了重复，窗口需要进行滑动，并将新元素添加进数组
+最终变为[ 'v', 'd' ] */
+lengthOfLongestSubstring('advdf');
