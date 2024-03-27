83f61fe05c4d764a65458e2c78cdffff5a52c9b8
songyx1997 <songyunxiang1997@163.com>
2024-03-27 17:29:06 +0800

    滑动窗口最大值，未解决

---
+++ leetcode/栈、队列/6.滑动窗口最大值.js
@@ -0,0 +1,71 @@
+// 输入：nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+// 输出：[3, 3, 5, 5, 6, 7]
+// 解释：
+// 滑动窗口的位置                最大值        单调队列
+// --------------------         ------        -------
+// [1  3  1]  2   0   5           3             [3,1]  
+//  1 [3  1   2]  0   5           3             [3,2]  // 1.往队尾加入元素，若队尾的元素小于当前待加入元素，则弹出（保持队列的单调性）
+//  1  3 [1   2   0]  5           2             [2,0]  // 2.窗口往右移动（窗口弹出3，这个值等于队列[3,2]的队头，队列弹出队头变成[2]）
+//  1  3  1  [2   0   5]          5              [5]
+
+// 该题目的思想是维护一个单调（递减）队列
+// 从上面的单调队列可以看出，最大值就是队列的第一个元素
+
+// 单调（递减）队列
+class MyQueue {
+    constructor() {
+        this.arr = [];
+    }
+    push(el) {
+        let max = this.peek();
+        // 1.往队尾加入元素，若队尾的元素小于当前待加入元素，则弹出（保持队列的单调性）
+        if (max) {
+            if (max < el) {
+                this.arr = [el];
+            } else {
+                while (this.arr[this.arr.length - 1] < el) {
+                    this.arr.pop();
+                }
+                this.arr.push(el);
+            }
+        } else {
+            this.arr.push(el);
+        }
+    }
+    pop(el) {
+        // 2.只有当窗口弹出的元素与队头元素相等时，才弹出
+        let max = this.peek();
+        if (max) {
+            if (max === el) {
+                return this.arr.shift();
+            }
+        }
+    }
+    peek() {
+        return this.arr[0];
+    }
+}
+
+const maxSlidingWindow = function (nums, k) {
+    // 初始化单调队列
+    let queue = new MyQueue();
+    for (let i = 0; i < k; i++) {
+        queue.push(nums[i]);
+    }
+    let result = [];
+    if (queue.peek()) {
+        result.push(queue.peek());
+    }
+    for (let i = k; i < nums.length; i++) {
+        // nums[i - k]为待出窗口的数据
+        queue.pop(nums[i - k]);
+        // nums[i]为待入队的元素
+        queue.push(nums[i]);
+        if (queue.peek()) {
+            result.push(queue.peek());
+        }
+    }
+    return result;
+};
+
+console.log(maxSlidingWindow([1, 3, 1, 2, 0, 5], 3));
